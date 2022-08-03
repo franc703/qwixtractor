2dc6f45a6900e8ffb4fa8655e3a8704c814e8a9a
Rodrigo Franco <franc703@umn.edu>
2022-08-03 00:02:16 -0500

    docs: created docstrings for package functions

--- src/qwixtractor/qwixtractor.py
+++ src/qwixtractor/qwixtractor.py
@@ -7,6 +7,25 @@ pbar = ProgressBar()
 
 # Construct function to make api call for qwi
 def get_qwi(state = '24', key = "your api key", the_vars = ['Emp', 'TurnOvrS', 'EarnBeg']):
+    """ Get data from Quarterly Workforce Indicators
+
+    Parameters
+    ----------
+    state: str
+        a string with the state fips code (i.e.: '27' for Minnesota, '01' for Alabama).
+    key: str
+        your api key, you should get it from census: https://api.census.gov/data/key_signup.html
+    the_vars: list
+        a list of the variables you want to request from QWI
+
+    Returns
+    -------
+    A pandas data frame for the requested state QWI data. pd.DataFrame object
+
+    Examples
+    --------
+    >>> get_qwi(state = '24', key = 'your api key', the_vars= ['Emp', 'TurnOverS', 'EarnBeg'])
+    """
     HOST = 'http://api.census.gov/data/timeseries/qwi/sa?'
     get_vars = the_vars
     predicates = {}
@@ -26,6 +45,27 @@ def get_qwi(state = '24', key = "your api key", the_vars = ['Emp', 'TurnOvrS', '
 
 
 def create_qwi_df(states = "states", col_names = "col_names", my_key = "your key"):
+    """ Get QWI data for a list of states and concatenate into a pandas dataframe
+
+    Parameters
+    ----------
+    states: str or list
+        if you need a single state QWI data input it as a string. If you need more than one state, use a list
+    col_names: list
+        a list with strings defining the names of the new column. This is evolving and might be modified
+    my_key: str
+        a string with the api key you get from census: https://api.census.gov/data/key_signup.html
+
+    Returns
+    -------
+    A pandas dataframe: pd.DataFrame object
+
+    Examples
+    --------
+    >>> col_names = ['emp', 'turn_overs', 'earn_beg', 'sex', 'age_group', 'industry', 'owner_code','quarter', 'season_adj', 'state', 'county']
+    >>> api_key = "Your api key"
+    >>> create_qwi_df(states = ['01', '27'], col_names = col_names, my_key = api_key)
+    """
     appended_data = []
     if isinstance(states, list):
         print("Getting your states...")
